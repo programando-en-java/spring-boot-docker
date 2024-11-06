dd7617dc733b9a647047dc1af16e57fef7d4ddc7
Ricardo Romero <ricardoromebeni@gmail.com>
2024-11-06 16:34:58 +0100

    first commit

---
+++ src/main/java/com/programandoenjava/spring_boot_docker/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.spring_boot_docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
